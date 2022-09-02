97d994ee9babd31190a2053937172ebee9ccca4b
Ricardo Hernandez <rmhm.photography@gmail.com>
2022-09-01 21:51:16 -0400

    Added css etc

--- public/js/main.js
+++ public/js/main.js
@@ -67,3 +67,10 @@ async function markIncomplete(){
         console.log(err)
     }
 }
+
+//test for change background -Kaleb
+function changeColor(event)
+{
+    var color = event.value;
+    document.getElementsByTagName('BODY')[0].style.backgroundColor=color;
+}
